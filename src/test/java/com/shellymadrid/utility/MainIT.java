af3393b137b8cb4be6e9e2c95702ab070d1a8ec0
Jesus Redon <jesusrrm@gmail.com>
2024-07-03 18:02:34 +0200

    First commit

---
+++ src/test/java/com/shellymadrid/utility/MainIT.java
@@ -0,0 +1,8 @@
+package com.shellymadrid.utility;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class MainIT extends MainTest {
+    // Execute the same tests but in packaged mode.
+}
